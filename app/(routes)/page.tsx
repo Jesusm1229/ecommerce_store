4077e05d9cc1f4048fce57fe70c2c43de9c99ac8
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-12-15 15:44:49 -0400

    commit

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,20 @@
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
-import Billboard from "@/components/ui/billboard";
+import BillboardUI from "@/components/ui/billboard";
+/* import Billboard from "@/components/ui/billboard"; */
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("9fc06433-b22e-4e6e-851b-9f55a90f4751");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
+        <BillboardUI
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
